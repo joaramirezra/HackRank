f74c4df90882331866007669c1ab8819f295d955
Johan <joaramirezra@unal.edu.co>
2018-09-04 22:36:05 -0500

    Dia 9 no he terminado el reto pero falta muy poco, pancracia me pide dormir

---
+++ JavaScript/Dia 9/js/binaryCalculator.js
@@ -0,0 +1,12 @@
+let btn1 = document.getElementById("btn0");
+let btn2 = document.getElementById("btn1");
+let btn3 = document.getElementById("btnClr");
+let btn4 = document.getElementById("btnEql");
+let btn5 = document.getElementById("btnSum");
+let btn6 = document.getElementById("btnSub");
+let btn7 = document.getElementById("btnMul");
+let btn8 = document.getElementById("btnDiv");
+let btn9 = document.getElementById("res");
+
+
+btn1.addEventListener("click",btn9.innerHTML = "1");
