68fa6312abf5010770a39c62a76312e9d11eeced
Johan <joaramirezra@unal.edu.co>
2018-09-05 16:24:52 -0500

    dia 10 con javascript terminado aunque aun me quedan dudas con respecto a como se relacionan los metodos seguire avanzando

--- JavaScript/Dia 9/js/binaryCalculator.js
+++ JavaScript/Dia 9/js/binaryCalculator.js
@@ -1,12 +1,37 @@
-let btn1 = document.getElementById("btn0");
-let btn2 = document.getElementById("btn1");
-let btn3 = document.getElementById("btnClr");
-let btn4 = document.getElementById("btnEql");
-let btn5 = document.getElementById("btnSum");
-let btn6 = document.getElementById("btnSub");
-let btn7 = document.getElementById("btnMul");
-let btn8 = document.getElementById("btnDiv");
-let btn9 = document.getElementById("res");
-
-
-btn1.addEventListener("click",btn9.innerHTML = "1");
+   const btns = document.getElementById('btns');
+    const res = document.getElementById('res');
+    let temp1, temp2, operator;
+    
+    btns.addEventListener('click', (e) => {
+        
+        let target = e.target.innerText;        
+        if (target == 'C') {
+            return res.innerText = '';
+        } else if(target == '=') {
+            let index = res.innerText.indexOf(operator);
+            temp2 = res.innerText.slice(index + 1);
+            switch (operator) {
+                case '+':
+                total = parseInt( temp1, 2 ) + parseInt( temp2, 2 );
+                break;
+                case '-':
+                total = parseInt( temp1, 2 ) - parseInt( temp2, 2 );
+                break;
+                case '*':
+                total = parseInt( temp1, 2 ) * parseInt( temp2, 2 );
+                break;
+                case '/':
+                total = parseInt( temp1, 2 ) / parseInt( temp2, 2 );
+                break;
+            }
+            res.innerText = total.toString(2);
+        } else {
+            if(target == '+' || target == '-' || target == '*' || target == '/') {
+                temp1 = res.innerText;
+                operator = target;
+                res.innerText += target;
+            } else {
+                res.innerText += target;
+            }
+        } 
+    });
